c1310fc2e5c07757a9ce369dd42e2732dc96b0dc
GabrielP1711 <gaps.admon17@gmail.com>
2025-11-21 11:35:06 -0500

    tabla componentes agregada

--- backend/componentes/admin.py
+++ backend/componentes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from componentes.models import Componente
 
 # Register your models here.
+admin.site.register(Componente)
--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'horario',
     'usuarios',
+    'componentes',
     'sedes',
     'facultades',
     'programas',
