ac7f6512ed01fcc7b5569a7be0de2614e8e747b1
armandod-perezb <armandod-perezb@unilibre.edu.co>
2025-11-27 23:37:23 -0500

    registro de la nueva table en interfaz admin

--- backend/espacios/admin.py
+++ backend/espacios/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from espacios.models import EspacioFisico
+from espacios.models import EspacioPermitido
 
 # Register your models here.
 admin.site.register(EspacioFisico)
+admin.site.register(EspacioPermitido)
